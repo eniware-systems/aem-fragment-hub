00e9dee5998c469e565e626f98ad173756162718
Marcus von Keil <m.vonkeil@eniware-systems.de>
2022-08-22 17:26:25 +0200

    Fixed XML formatting

--- de.enithing.contenthub.generator.core/src/main/java/de/enithing/contenthub/generator/contentfragment/model/field/FragmentReferenceTypeGenerator.java
+++ de.enithing.contenthub.generator.core/src/main/java/de/enithing/contenthub/generator/contentfragment/model/field/FragmentReferenceTypeGenerator.java
@@ -1,61 +1,61 @@
 package de.enithing.contenthub.generator.contentfragment.model.field;
 
+import de.enithing.contenthub.generator.GeneratorConfiguration;
+import de.enithing.contenthub.generator.util.JcrUtils;
+import de.enithing.contenthub.generator.util.VelocityUtils;
+import de.enithing.contenthub.model.contentfragment.corefields.FragmentReference;
+import org.apache.velocity.VelocityContext;
+
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.apache.velocity.VelocityContext;
-
-import de.enithing.contenthub.generator.GeneratorConfiguration;
-import de.enithing.contenthub.generator.util.JcrUtils;
-import de.enithing.contenthub.generator.util.VelocityUtils;
-import de.enithing.contenthub.model.contentfragment.corefields.FragmentReference;
-import de.enithing.contenthub.model.contenthub.util.ContextUtils;
-
 public class FragmentReferenceTypeGenerator extends ContentFragmentMultiFieldTypeGenerator<FragmentReference> {
 
-	public FragmentReferenceTypeGenerator(GeneratorConfiguration cfg) {
-		super(cfg);
-	}
+    public FragmentReferenceTypeGenerator(GeneratorConfiguration cfg) {
+        super(cfg);
+    }
 
-	@Override
-	public void populateAttribs(FragmentReference element, VelocityContext ctx, Map<String, Object> attribs) {
-		attribs.put("nameSuffix", "contentReference");
-		attribs.put("filter", "hierarchy");
+    @Override
+    public void populateAttribs(FragmentReference element, VelocityContext ctx, Map<String, Object> attribs) {
+        attribs.put("nameSuffix", "contentReference");
+        attribs.put("filter", "hierarchy");
 
-		List<Path> allowedModelPaths = element.getAllowedModels().stream()
-				.map(mdl -> VelocityUtils.replace(mdl.getPath(), ctx)).collect(Collectors.toList());
-		attribs.put("fragmentmodelreference", allowedModelPaths);
+        List<Path> allowedModelPaths = element.getAllowedModels().stream()
+                .map(mdl -> VelocityUtils.replace(mdl.getPath(), ctx)).collect(Collectors.toList());
+        attribs.put("fragmentmodelreference", allowedModelPaths);
 
-		attribs.put("allowNew", element.isAllowFragmentCreation());
-		attribs.put("required", JcrUtils.toFlag(element.isRequired()));
-		attribs.put("rootPath", element.getRootPath());
-	}
+        attribs.put("allowNew", element.isAllowFragmentCreation());
+        attribs.put("required", JcrUtils.toFlag(element.isRequired()));
+        if (element.getRootPath() != null) {
+            attribs.put("rootPath", element.getRootPath().getPath().toString());
+        }
+    }
 
-	@Override
-	public void getSharedFieldAttribs(Set<String> attribs) {
-		attribs.add("fragmentmodelreference");
-	}
+    @Override
+    public void getSharedFieldAttribs(Set<String> attribs) {
+        attribs.add("fragmentmodelreference");
+    }
 
-	@Override
-	public String getMultiResourceType(FragmentReference unused) {
-		return "dam/cfm/models/editor/components/fragmentreference/multifield";
-	}
+    @Override
+    public String getMultiResourceType(FragmentReference unused) {
+        return "dam/cfm/models/editor/components/fragmentreference/multifield";
+    }
 
-	@Override
-	public String getResourceType(FragmentReference unused) {
-		return "dam/cfm/models/editor/components/fragmentreference";
-	}
+    @Override
+    public String getResourceType(FragmentReference unused) {
+        return "dam/cfm/models/editor/components/fragmentreference";
+    }
 
-	@Override
-	public String getValueType(FragmentReference unused) {
-		return "string/content-fragment";
-	}
+    @Override
+    public String getValueType(FragmentReference unused) {
+        return "string/content-fragment";
+    }
 
-	@Override
-	public String getMetaType(FragmentReference unused) {
-		return "fragment-reference";
-	}
+    @Override
+    public String getMetaType(FragmentReference unused) {
+        return "fragment-reference";
+    }
 }
