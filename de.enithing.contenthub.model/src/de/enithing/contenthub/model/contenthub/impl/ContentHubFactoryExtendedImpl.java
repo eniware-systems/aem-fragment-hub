1070f57aa72f03171e7a4cecef3754c3907b8646
Marcus von Keil <m.vonkeil@eniware-systems.de>
2022-09-06 17:27:34 +0200

    Fixed missing EPackage in extended factory

--- de.enithing.contenthub.model/src/de/enithing/contenthub/model/contenthub/impl/ContentHubFactoryExtendedImpl.java
+++ de.enithing.contenthub.model/src/de/enithing/contenthub/model/contenthub/impl/ContentHubFactoryExtendedImpl.java
@@ -1,4 +1,5 @@
 /**
+ *
  */
 package de.enithing.contenthub.model.contenthub.impl;
 
@@ -7,22 +8,27 @@ import java.nio.file.Path;
 import org.eclipse.emf.ecore.EDataType;
 
 import de.enithing.contenthub.model.contenthub.Package;
+import de.enithing.contenthub.model.contenthub.ContentHubPackage;
 
 /**
  * Extensions for the implementation of the model factory
  */
 public class ContentHubFactoryExtendedImpl extends ContentHubFactoryImpl {
-	/**
-	 * Creates a path from a string. Used for operating with Paths on ECore model
-	 */
-	@Override
-	public Path createPathFromString(EDataType eDataType, String initialValue) {
-		return Path.of(initialValue);
-	}
-	
-	@Override
-	public Package createPackage() {
-		return new ExtendedPackageImpl();
-	}
-	
+    ContentHubFactoryExtendedImpl() {
+        setEPackage(ContentHubPackage.eINSTANCE);
+    }
+
+    /**
+     * Creates a path from a string. Used for operating with Paths on ECore model
+     */
+    @Override
+    public Path createPathFromString(EDataType eDataType, String initialValue) {
+        return Path.of(initialValue);
+    }
+
+    @Override
+    public Package createPackage() {
+        return new ExtendedPackageImpl();
+    }
+
 } // ContentHubFactoryImpl
