d7b9f3a37d4e21505f3003a43e17efa07a3ee6ee
Marcus von Keil <m.vonkeil@eniware-systems.de>
2022-09-14 17:24:49 +0200

    Fixed scripts, added run-editor script

--- de.enithing.fragmenthub.generator.cli/src/main/java/de/enithing/fragmenthub/generator/cli/Main.java
+++ de.enithing.fragmenthub.generator.cli/src/main/java/de/enithing/fragmenthub/generator/cli/Main.java
@@ -29,7 +29,7 @@ public class Main {
     }
 
     private static String getExecutableName() {
-        return "afm-generate";
+        return "afh-generate";
     }
 
     private static Option getHelpOption() {
